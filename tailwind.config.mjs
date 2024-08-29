2862ab5a4056d42584f251ae81d519fe9f43c330
Nefta Hernandez <neftaligow01@gmail.com>
2024-08-29 17:22:53 -0600

    feat: 🎸 Landing page
    Almost everything about Landing page is done, still missing some colors
    and sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "yellow-primary": "#ffdd00",
+        "purple-primary": "#842a80",
+        "blue-primary": "#009eff",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
